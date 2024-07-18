e5d0951153ffe1ef80cf867324f75411673492ee
Notebook <15nIk5Ita3@gmail.com>
2024-07-18 07:04:40 -0700

    changed

--- main.cpp
+++ main.cpp
@@ -2,75 +2,76 @@
 #include <cmath>
 int real_string_hash(std::string& str, int p, int n)
 {
-    uint64_t member = 0;
-    uint64_t current_p = 1;
-    for(int i = 0; i < str.length(); i++)
+    uint32_t member = 0;
+    uint32_t current_p = 1;
+    for (int i = 0; i < str.length(); i++)
     {
-        if(i == 0)
+        if (i == 0)
         {
-        member += static_cast< uint64_t>(str[i]);
+            member += static_cast<uint32_t>(str[i]);
         }
         else
         {
             current_p *= p;
-            member += static_cast< uint64_t>(str[i]) * current_p;
+            member += static_cast<uint32_t>(str[i]) * current_p;
         }
 
     }
-    return static_cast<int>(member%n);
+    return static_cast<int>(member % n);
 }
 
 bool cheack_p(int p)
 {
- if(p<=1)
- {
-    return false;
- }
- if(p <= 3 )
- {
-      return true;
- }
+    if (p <= 1)
+    {
+        return false;
+    }
+    if (p <= 3)
+    {
+        return true;
+    }
 
- if( p % 2 == 0 || p % 3 == 0){
-     return false;
- }
+    if (p % 2 == 0 || p % 3 == 0) {
+        return false;
+    }
 
- for ( int i = 5; i <= std::sqrt(p); i+=2)
- {
- if(p % i == 0)
- {
-     return false;
- }
- }
-return true;
+    for (int i = 5; i <= std::sqrt(p); i += 2)
+    {
+        if (p % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 int main()
 {
 
-   int p;
-   while(true)
-   {
-    std::cout << "Enter p\n";
-    std::cin >> p;
-    bool answer = cheack_p(p);
-    if(answer)
+    int p;
+    while (true)
     {
-        break;
-    }}
+        std::cout << "Enter p\n";
+        std::cin >> p;
+        bool answer = cheack_p(p);
+        if (answer)
+        {
+            break;
+        }
+    }
     std::cout << "Enter n\n";
     int n;
 
     std::cin >> n;
-    while(true)
+    while (true)
     {
-    std::string str;
-    std::cout << "Enter str\n";
-    std::cin >> str;
-    std::cout <<"Hash str " << str <<" " << real_string_hash(str,p,n) << std::endl;
-    if (str == "exit")
-    {
-        break;
-    }
+        std::string str;
+        std::cout << "Enter str\n";
+        std::cin >> str;
+        std::cout << "Hash str " << str << " " << real_string_hash(str, p, n) << std::endl;
+        if (str == "exit")
+        {
+            break;
+        }
     }
 
     return 0;
